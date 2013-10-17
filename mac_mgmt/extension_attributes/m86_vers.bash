881eddc33560c01c8fa9f2cb0ca27bfc77f21c17
Ryan Manly <ryan.manly@gmail.com>
2013-10-17 08:19:02 -0500

    update script for version 2.0.31 of M86Authenticator

--- mac_mgmt/extension_attributes/m86_vers.bash
+++ mac_mgmt/extension_attributes/m86_vers.bash
@@ -2,10 +2,10 @@
 
 m86_vers=$(/Applications/M86Authenticator/m86authenticator -version | awk 'BEGIN { FS="." } NR == 1 { print $3; exit }')
 
-if [[ "${m86_vers}" -eq 10 ]]; then
-    echo "<result>up2date</result>"
-elif [[ "${m86_vers}" -lt 10 ]]; then
-    echo "<result>NO</result>"
+if [[ "${m86_vers}" == 31 ]]; then
+    echo "<result>NEW</result>"
+elif [[ "${m86_vers}" < 31 ]]; then
+    echo "<result>OLD</result>"
 else
     echo "<result>error - ${m86_vers}</result>"
 fi
