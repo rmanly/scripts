b192d6b724009d7ede2aff0e2e65b8e48aa456b8
Ryan Manly <ryan.manly@gmail.com>
2014-03-07 14:52:48 -0600

    first commit

---
+++ makecachescripts/makecache.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#kernalcache Builder
+#by Daniel Shane
+#v1.1
+
+# Check to make sure we are root
+if [ `id -u` != "0" ]
+then
+   echo "`basename $0` must be run as root"
+   exit 1
+fi
+
+if [ $# != "2" ]
+then
+   echo "`basename $0` must have a source drive and an output directory for the kernalcache file"
+   echo "e.g."
+   echo "`basename $0` / ~/Desktop/"
+   echo ""
+   exit 1
+fi
+
+#Remove unneeded KEXTS that are not need for NB to work
+mkdir /tmp/Extensions
+cp -r "$1"/System/Library/Extensions/* /tmp/Extensions
+rm -rf /tmp/Extensions/ATI*
+rm -rf /tmp/Extensions/AMD*
+rm -rf /tmp/Extensions/ATTO*
+kextcache -v 6 -arch x86_64 -K "$1"/mach_kernel -c "$2"/kernelcache /tmp/Extensions/
+rm -rf /tmp/Extensions
