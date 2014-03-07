a3791e98a02206472c11aa33c9886931ea13d43a
Ryan Manly <ryan.manly@gmail.com>
2014-03-07 15:19:33 -0600

    absolute path for safety / security

--- makecachescripts/makecache.sh
+++ makecachescripts/makecache.sh
@@ -21,8 +21,8 @@ fi
 #Remove unneeded KEXTS that are not need for NB to work
 mkdir /tmp/Extensions
 cp -r "$1"/System/Library/Extensions/* /tmp/Extensions
-rm -rf /tmp/Extensions/ATI*
-rm -rf /tmp/Extensions/AMD*
-rm -rf /tmp/Extensions/ATTO*
+/bin/rm -rf /tmp/Extensions/ATI*
+/bin/rm -rf /tmp/Extensions/AMD*
+/bin/rm -rf /tmp/Extensions/ATTO*
 kextcache -v 6 -arch x86_64 -K "$1"/mach_kernel -c "$2"/kernelcache /tmp/Extensions/
 rm -rf /tmp/Extensions
