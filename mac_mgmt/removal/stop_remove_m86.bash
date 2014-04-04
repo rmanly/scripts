6552bf67477bfefe4a18b3bb824df3690028197b
Ryan Manly <ryan.manly@gmail.com>
2014-04-04 09:22:14 -0500

    apostrophe
    :)

--- mac_mgmt/removal/stop_remove_m86.bash
+++ mac_mgmt/removal/stop_remove_m86.bash
@@ -3,7 +3,7 @@
 # exec > /var/log/m86remove.log 2>&1
 
 # Prompted by a question from Eric Wacker about removing M86 cleanly without rebooting or
-# forcing users to logout etc. Got idea from Gordon Davissons answer here
+# forcing users to logout etc. Got idea from Gordon Davisson's answer here
 # http://apple.stackexchange.com/questions/61901/is-there-a-way-to-load-a-launchagent-as-another-user
 
 # I keep running with the while because I have seen cases of multiple instances of the
