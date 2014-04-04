e45e9c788d797485eb308e96291160fd8fa3b35f
Ryan Manly <ryan.manly@gmail.com>
2014-04-04 09:17:26 -0500

    notes & comments

--- mac_mgmt/removal/stop_remove_m86.bash
+++ mac_mgmt/removal/stop_remove_m86.bash
@@ -2,6 +2,10 @@
 
 # exec > /var/log/m86remove.log 2>&1
 
+# Prompted by a question from Eric Wacker about removing M86 cleanly without rebooting or
+# forcing users to logout etc. Got idea from Gordon Davissons answer here
+# http://apple.stackexchange.com/questions/61901/is-there-a-way-to-load-a-launchagent-as-another-user
+
 while pgrep -q m86Authenticator; do
     m86_pid=$(pgrep -n m86Authenticator)
     m86_user=$(ps -p "$m86_pid" -o user=)
