88e7faee5cea544d6810ec558908797374887de9
Ryan Manly <ryan.manly@gmail.com>
2014-04-04 09:19:39 -0500

    full paths to command for precision and security etc.

--- mac_mgmt/removal/stop_remove_m86.bash
+++ mac_mgmt/removal/stop_remove_m86.bash
@@ -11,8 +11,8 @@
 while pgrep -q m86Authenticator; do
     m86_pid=$(pgrep -n m86Authenticator)
     m86_user=$(ps -p "$m86_pid" -o user=)
-    launchctl bsexec "$m86_pid" su "$m86_user" -c 'launchctl unload /Library/LaunchAgents/com.m86security.authenticator.plist'
+    /bin/launchctl bsexec "$m86_pid" su "$m86_user" -c 'launchctl unload /Library/LaunchAgents/com.m86security.authenticator.plist'
 done
 
-rm -rf /Applications/M86Authenticator
-rm /Library/LaunchAgents/com.m86security.authenticator.plist
+/bin/rm -rf /Applications/M86Authenticator
+/bin/rm /Library/LaunchAgents/com.m86security.authenticator.plist
