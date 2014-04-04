648332f96427a2709b6aaaae575355dc312f0bdd
Ryan Manly <ryan.manly@gmail.com>
2014-04-04 09:13:53 -0500

    initial commit

---
+++ mac_mgmt/removal/stop_remove_m86.bash
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+exec > /var/log/m86remove.log 2>&1
+
+while pgrep -q m86Authenticator; do
+    m86_pid=$(pgrep -n m86Authenticator)
+    m86_user=$(ps -p "$m86_pid" -o user=)
+    launchctl bsexec "$m86_pid" su "$m86_user" -c 'launchctl unload /Library/LaunchAgents/com.m86security.authenticator.plist'
+done
+
+rm -rf /Applications/M86Authenticator
+rm /Library/LaunchAgents/com.m86security.authenticator.plist
