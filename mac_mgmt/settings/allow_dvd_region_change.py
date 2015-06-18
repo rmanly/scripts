a9a95c846af043f302d849b409b06c4fe6143cf3
Ryan Manly <ryan.manly@gmail.com>
2015-06-18 12:14:40 -0500

    initial commit

---
+++ mac_mgmt/settings/allow_dvd_region_change.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import plistlib
+import subprocess
+
+def get_initial_settings():
+    proc = subprocess.Popen(['/usr/bin/security',
+            'authorizationdb',
+            'read',
+            'system.device.dvd.setregion.initial'],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    return stdout
+    
+def modify_plist():
+    settings = plistlib.readPlistFromString(get_initial_settings())
+    settings['class'] = 'allow'
+    settings['comment'] = 'Allows any user to change the DVD region \
+            code after it has been set the first time.'
+    settings['group'] = 'user'
+    return settings
+
+def write_sec_settings():
+    tmp_file = '/tmp/system.device.dvd.setregion.change.plist'
+    cmd = '/usr/bin/security authorizationdb write system.device.dvd.setregion.change <' + tmp_file
+    plistlib.writePlist(modify_plist(), tmp_file)
+    try:
+        subprocess.check_call(cmd, shell=True)
+    except subprocess.CalledProcessError as error:
+        if error.returncode == 255:
+            print 'Failed to write plist to authdb!'
+            print 'You must provide valid credentials!'
+        exit(error.returncode)
+
+write_sec_settings()
