ce2cc5c2894fdfdf0bd452bbff8d9432f9c55a63
Ryan Manly <ryan.manly@gmail.com>
2015-09-24 10:01:16 -0500

    subtree prep

--- mac_mgmt/removal/rm_com.apple.quarantine.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-while read -r -d $'\0' item; do
-    /usr/bin/xattr -s -d com.apple.quarantine "${item}"
-done < <(/usr/bin/find /Applications -xattrname com.apple.quarantine -print0)
