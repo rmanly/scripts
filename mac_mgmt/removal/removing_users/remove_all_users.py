fd7b52a528246125e7fae6cbd0a6aa3820340960
Ryan Manly <ryan.manly@gmail.com>
2016-02-11 15:35:02 -0600

    pylint and comments

--- mac_mgmt/removal/removing_users/remove_all_users.py
+++ mac_mgmt/removal/removing_users/remove_all_users.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python
+"""
+This script will delete the directories in /Users and remove users
+from dscl using the same path information
+"""
 
 import argparse
 import glob
 from shutil import rmtree
 from subprocess import Popen, PIPE
 
-user_dirs = glob.glob('/Users/*')
 
 def compile_ignore_list():
+    """
+    we don't want to remove Shared or the users and dirs passed with -i
+    """
     ignore_list = ['/Users/Shared']
 
     parser = argparse.ArgumentParser()
@@ -21,28 +27,40 @@ def compile_ignore_list():
 
     return ignore_list
 
-## Verify if no args.ignore. Maybe have a -Y flag to set
-## Change above to something like `if args.ignore and args.yes:`
+
 def confirm_delete_all():
+    """
+    Currently unused...thoughts...
+    Verify if no args.ignore. Maybe have a -Y flag to set
+    Change above to something like `if args.ignore and args.yes:`
+    """
     print "You did not specify anything from /Users to ignore."
     print "Do you REALLY want to delete ALL everything except /Users/Shared?"
 
+
 def del_user(user_path):
-    p = Popen(['/usr/bin/dscl', '/Local/Default', '-delete', user_path],
-                stdout=PIPE, stderr=PIPE)
-    _, stderr = p.communicate()
+    """
+    attempt to remove the user from dscl using path
+    """
+    proc = Popen(['/usr/bin/dscl', '/Local/Default', '-delete', user_path],
+                 stdout=PIPE, stderr=PIPE)
+    _, stderr = proc.communicate()
     if stderr:
         print 'Could not remove user ' + user_path.split('/')[-1] + \
                 ' because reasons.'
 
+
 def del_home(user_path):
+    """
+    delete paths while catching errors
+    """
     try:
         rmtree(user_path)
-    except OSError as e:
-        if e.errno == 2:
+    except OSError as err:
+        if err.errno == 2:
             # 'No such file or directory' error
             print user_path + ' does not exist!'
-        elif e.errno == 13:
+        elif err.errno == 13:
             # permission denied
             print 'Got permission error when attempting to remove ' + \
                     user_path + '.\nDid you run as root?'
@@ -52,8 +70,18 @@ def del_home(user_path):
             raise
 
 
-for path in user_dirs:
-    if path not in compile_ignore_list():
-        print path + " would've been deleted"
-        # del_home(path)
-        # del_user(path)
+def main():
+    """
+    for all the given user_dirs attempt to delete the dir and the user
+    """
+    user_dirs = glob.glob('/Users/*')
+
+    for path in user_dirs:
+        if path not in compile_ignore_list():
+            print path + " would've been deleted"
+            # del_home(path)
+            # del_user(path)
+
+
+if __name__ == '__main__':
+    main()
